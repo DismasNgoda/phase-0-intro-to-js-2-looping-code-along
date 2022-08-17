5475ffbc5b25d02230d6e7fc242d5b0bf2a31400
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-08-17 17:36:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards (names, event) {
+    let emptyArray = [];
+    for (let i = 0; i < names.length; i++) {
+    emptyArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    debugger;
+    // console.log(i);
+    };
+    return emptyArray;
+};
+writeCards (["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown () {
+    let n = 10;
+        while (n>=0){
+            console.log(n);
+            n--;
+        };
+       
+    };
+   countDown();
+   
